5fd8114626b634dc74a0aacb2ef8aede801f51cd
kazuki-afb <kauzki_afb@yahoo.co.jp>
2020-04-16 05:38:12 +0900

    comments_new_setting

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   validates :name, :ability_type_id, :ability_person, presence: true
-  
+  # 実の名前が重複しないようにする
+  validates :name, uniqueness: true
   has_many :comments
   belongs_to :ability_type
 end
