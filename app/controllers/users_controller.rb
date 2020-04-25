6a356bce07f29bc0f16727a2bdd332c3fdd2621d
kazuki-afb <kauzki_afb@yahoo.co.jp>
2020-04-25 19:05:47 +0900

    search_viwe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path	
+    redirect_to new_user_session_path
   end
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root "items#index"
+  root "items#search"
   resources :users, only: [:edit, :update,:destroy]
   resources :items do
     resources :comments, only: [:index, :new, :create]
+    collection do
+      get 'search'
+    end
   end
   resources :images, only: [:new, :create]
 end
