1c25c1d6739485090e2fd590b202d9b1463625a7
kazuki-afb <kauzki_afb@yahoo.co.jp>
2020-05-19 21:06:55 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   validates :name, :ability_type_id, :ability_person, presence: true
   # 実の名前が重複しないようにする
-  validates :name, uniqueness: true, if: -> {race.blank?}
-  validates :race, uniqueness: true
+  # validates :name, uniqueness: true, if: -> {race.blank?}
+  # validates :race, uniqueness: true
   has_many :comments
   has_many :images
   belongs_to :ability_type
